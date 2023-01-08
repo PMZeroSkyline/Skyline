ae25895777485410f43e825aa57725efe923d928
pmzeroskyline <guzhang0@gmail.com>
2023-01-09 01:03:36 +0800

    smoke

--- main.cpp
+++ main.cpp
@@ -10,8 +10,7 @@
 #include "material.h"
 #include "aarect.h"
 #include "box.h"
-#include <thread>
-
+#include "constant_medium.h"
 
 color ray_color(const ray &r, const color &background, const hittable &world, int depth)
 {
@@ -140,9 +139,6 @@ hittable_list cornell_box() {
 	auto green = make_shared<lambertian>(color(.12, .45, .15));
 	auto light = make_shared<diffuse_light>(color(15, 15, 15));
 
-	objects.add(make_shared<box>(point3(130, 0, 65), point3(295, 165, 230), white));
-	objects.add(make_shared<box>(point3(265, 0, 295), point3(430, 330, 460), white));
-
     objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
     objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
     objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
@@ -150,6 +146,43 @@ hittable_list cornell_box() {
     objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
     objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
 	
+	shared_ptr<hittable> box1 = make_shared<box>(point3(0), point3(165, 330, 165), white);
+	box1 = make_shared<rotate_y>(box1, 15);
+	box1 = make_shared<translate>(box1, vec3(264, 0, 295));
+	objects.add(box1);
+	shared_ptr<hittable> box2 = make_shared<box>(point3(0), point3(165), white);
+	box2 = make_shared<rotate_y>(box2, -18);
+	box2 = make_shared<translate>(box2, vec3(130, 0, 64));
+	objects.add(box2);
+
+    return objects;
+}
+hittable_list cornell_box_smoke() {
+    hittable_list objects;
+
+    auto red   = make_shared<lambertian>(color(.65, .05, .05));
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+	auto green = make_shared<lambertian>(color(.12, .45, .15));
+	auto light = make_shared<diffuse_light>(color(7));
+
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<xz_rect>(113, 443, 127, 432, 554, light));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+    objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+	
+	shared_ptr<hittable> box1 = make_shared<box>(point3(0), point3(165, 330, 165), white);
+	box1 = make_shared<rotate_y>(box1, 15);
+	box1 = make_shared<translate>(box1, vec3(264, 0, 295));
+	box1 = make_shared<constant_medium>(box1, 0.01, color(0));
+	objects.add(box1);
+	shared_ptr<hittable> box2 = make_shared<box>(point3(0), point3(165), white);
+	box2 = make_shared<rotate_y>(box2, -18);
+	box2 = make_shared<translate>(box2, vec3(130, 0, 64));
+	box2 = make_shared<constant_medium>(box2, 0.01, color(1));
+	objects.add(box2);
+
     return objects;
 }
 
@@ -212,6 +245,13 @@ int main(int argc, char **argv)
 			break;
 		case 6:
 			world = cornell_box();
+			background = color(0);
+			lookfrom = point3(278, 278, -800);
+			lookat = point3(278, 278, 0);
+			vfov = 40.0;
+			break;
+		case 7:
+			world = cornell_box_smoke();
 			background = color(0);
 			lookfrom = point3(278, 278, -800);
 			lookat = point3(278, 278, 0);
--- material.h
+++ material.h
@@ -113,4 +113,19 @@ public:
     }
 };
 
+class isotropic : public material
+{
+public:
+    shared_ptr<texture> albedo;
+
+    isotropic(color c) : albedo(make_shared<solid_color>(c)){}
+    isotropic(shared_ptr<texture> a) : albedo(a){}
+
+    virtual bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override
+    {
+        scattered = ray(rec.p, random_in_unit_sphere(), r_in.tm);
+        attenuation = albedo->value(rec.u, rec.v, rec.p);
+        return true;
+    }
+};
 #endif
